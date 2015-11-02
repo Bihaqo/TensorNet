5c222454dd1b8637ef6e5bdbfbfc7b507dbfa643
Alexander Novikov <novikov@bayesgroup.ru>
2015-11-02 13:23:20 +0300

    Fix the implementation of the TT layer and add comments

--- src/python/ttlayer.py
+++ src/python/ttlayer.py
@@ -17,7 +17,9 @@ class TTLayer(lasagne.layers.Layer):
         Examples
         --------
         """
-    def __init__(self, incoming, input_shape, output_shape, ranks, cores=lasagne.init.Normal(0.01), nonlinearity=lasagne.nonlinearities.rectify, **kwargs):
+    def __init__(self, incoming, input_shape, output_shape, ranks,
+                 cores=lasagne.init.Normal(0.01),
+                 nonlinearity=lasagne.nonlinearities.rectify, **kwargs):
         super(TTLayer, self).__init__(incoming, **kwargs)
         self.tt_input_shape = input_shape
         self.tt_output_shape = output_shape
@@ -32,14 +34,19 @@ class TTLayer(lasagne.layers.Layer):
         # iterations (see https://github.com/Theano/Theano/issues/2127),
         # so we are using `for loop` instead.
         res = input
-        core_idx = 0
-        for k in range(self.num_dim):
-            curr_shape = (self.tt_ranks[k] * self.tt_input_shape[k], self.tt_output_shape[k] * self.tt_ranks[k + 1])
-            curr_core = self.cores_arr[core_idx:core_idx+T.prod(curr_shape)].reshape(curr_shape)
+        core_arr_idx = 0
+        for k in range(self.num_dim - 1, -1, -1):
+            # res is of size o_k+1 x ... x o_d x batch_size x i_1 x ... x i_k-1 x i_k x r_k+1
+            curr_shape = (self.tt_input_shape[k] * self.tt_ranks[k + 1], self.tt_ranks[k] * self.tt_output_shape[k])
+            curr_core = self.cores_arr[core_arr_idx:core_arr_idx+T.prod(curr_shape)].reshape(curr_shape)
             res = T.dot(res.reshape((-1, curr_shape[0])), curr_core)
-#             res = res.reshape((-1, self.output_shape[k])), transpose
-            core_idx += T.prod(curr_shape)
+            # res is of size o_k+1 x ... x o_d x batch_size x i_1 x ... x i_k-1 x r_k x o_k
+            res = T.transpose(res.reshape((-1, self.tt_output_shape[k])))
+            # res is of size o_k x o_k+1 x ... x o_d x batch_size x i_1 x ... x i_k-1 x r_k
+            core_arr_idx += T.prod(curr_shape)
+        # res is of size o_1 x ... x o_d x batch_size
         res = res.reshape((input.shape[0], -1))
+        # res is of size batch_size x o_1 x ... x o_d
         return self.nonlinearity(res)
 
     def get_output_shape_for(self, input_shape):
